78a6d3db16c705fb8a7738da0315c30c0ac8daf1
palaciosyost <palaciosyostin903@gmail.com>
2025-12-18 10:35:36 -0500

    paginacion de productos

--- model/tag.py
+++ model/tag.py
@@ -1,45 +1,43 @@
-from odoo import models, fields, api
-
-
+from odoo import models, api
 
 class TagImageProduct(models.Model):
     _inherit = 'product.tag'
 
-    def set_image_product(self, batch_size=2000, commit_each_batch=False):
-        """
-        Copia la imagen del tag a image_1920 de todos los product.template que tengan este tag.
-        - batch_size: tamaño del lote (1000–5000 suele ir bien)
-        - commit_each_batch: úsalo en cron/procesos largos (no en botón normal)
-        """
+    def set_image_product(self, batch_size=300, commit_each_batch=True):
         self.ensure_one()
 
-        # imagen binaria del tag
         tag_img = self.image
         if not tag_img:
             return 0
 
-        ProductT = self.env['product.template'].with_context(prefetch_fields=False)
+        ProductT = self.env['product.template'].with_context(
+            prefetch_fields=False,
+            tracking_disable=True,
+            mail_notrack=True,
+        )
 
         last_id = 0
         total = 0
 
         while True:
-            domain = [
-                ('product_tag_ids', 'in', self.id),
-                ('id', '>', last_id),
-            ]
-            batch = ProductT.search(domain, order='id', limit=batch_size)
+            batch = ProductT.search(
+                [('product_tag_ids', 'in', self.id), ('id', '>', last_id)],
+                order='id',
+                limit=batch_size
+            )
             if not batch:
                 break
 
-            # 1 write por lote (mucho más rápido)
-            batch.write({'image_1920': tag_img})
+            # Savepoint para que si algo falla no mate todo
+            with self.env.cr.savepoint():
+                batch.write({'image_1920': tag_img})
 
             total += len(batch)
             last_id = batch[-1].id
 
-            # Para crons/procesos largos ayuda a no reventar RAM/locks
             if commit_each_batch:
                 self.env.cr.commit()
+                # MUY importante en procesos largos: libera cache ORM
+                self.env.invalidate_all()
 
         return total
